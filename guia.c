082fe2251d09606c7d800a7226ee23c51c0f4569
Rasberry Pie <josue.ad.95@gmail.com>
2020-11-12 19:06:53 -0600

    new update

--- guia.c
+++ guia.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int numbers[];
 
 void leerArchivo(){
   FILE * fp;
@@ -12,18 +13,29 @@ void leerArchivo(){
   fp = fopen("numbers.txt", "r");
   if (fp == NULL)
      exit(EXIT_FAILURE);
-     
-   while (getline(&line, &len, fp) != -1) {
-      number = atoi(line);
-      printf("%i", number);
+
+   int i = 0;
+   while (i < 2048){
+      getline(&line, &len, fp);
+      numbers[i++] = atoi(line);
    }
 
    fclose(fp);
    if (line)
       free(line);
-   exit(EXIT_SUCCESS);  
+   exit(EXIT_SUCCESS);
+}
+
+void ordenar(){
+   int i, j;
+   for(i = 0; i < 2048; i++){
+      for(j = i+1; < 2047; j++){
+      
+      }
+   }
 }
 
 int main(void){
     leerArchivo();
+    ordenar();
 }
